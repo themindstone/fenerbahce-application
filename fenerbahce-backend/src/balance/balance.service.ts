8d2ed3a9c205c72100eb933d747c337c62f08e03
Enes İnce <inceenes10@gmail.com>
2022-09-22 17:40:20 +0300

    fix

--- fenerbahce-backend/src/balance/balance.service.ts
+++ fenerbahce-backend/src/balance/balance.service.ts
@@ -32,6 +32,7 @@ export class BalanceService {
             .createQueryBuilder()
             .select("*")
             .where("auction_id = :auctionId", { auctionId })
+            .andWhere("user_adderss = :userAddress", { userAddress })
             .execute();
         if (res.length === 0) {
             return null;
