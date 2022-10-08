bd2ec0890b1acc0b2afbc718a1c9980deb3025dd
Enes İnce <inceenes10@gmail.com>
2022-10-08 10:39:58 +0300

    deploy.ts updated

--- fenerbahce-contract/hardhat.config.ts
+++ fenerbahce-contract/hardhat.config.ts
@@ -5,48 +5,49 @@ import "@nomicfoundation/hardhat-toolbox";
 // dotenv.config()
 
 interface ENV {
-	DEPLOYER_PRIVATE_KEY: string;
-	ALCHEMY_API_KEY: string;
-};
+  DEPLOYER_PRIVATE_KEY: string;
+  ALCHEMY_API_KEY: string;
+}
 
 const getConfig = (): ENV => {
-	return {
-		DEPLOYER_PRIVATE_KEY: process.env.DEPLOYER_PRIVATE_KEY as string,
-		ALCHEMY_API_KEY: process.env.ALCHEMY_API_KEY as string
-	};
+  return {
+    DEPLOYER_PRIVATE_KEY: process.env.DEPLOYER_PRIVATE_KEY as string,
+    ALCHEMY_API_KEY: process.env.ALCHEMY_API_KEY as string,
+  };
 };
 
 const env = getConfig();
+console.log(env)
 
 // console.log(env.DEPLOYER_PRIVATE_KEY)
 const config: HardhatUserConfig = {
-	solidity: "0.8.9",
-	networks: {
-		local: {
-			url: "http://127.0.0.1:8545",
-			gasPrice: 225000000000,
-			chainId: 8178,
-			accounts: [
-				"ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
-			],
-		},
-		// fuji: {
-		// 	url: "https://api.avax-test.network/ext/bc/C/rpc",
-		// 	gasPrice: 225000000000,
-		// 	chainId: 43113,
-		// 	accounts: [env.DEPLOYER_PRIVATE_KEY]
-		// },
-		goerli: {
-			url: `https://eth-goerli.alchemyapi.io/v2/${env.ALCHEMY_API_KEY}`,
-			accounts: [env.DEPLOYER_PRIVATE_KEY]
-		  },
-		mainnet: {
-			url: "https://api.avax.network/ext/bc/C/rpc",
-			gasPrice: 225000000000,
-			chainId: 43114,
-			accounts: [env.DEPLOYER_PRIVATE_KEY],
-		},
-	},
+  solidity: "0.8.9",
+  networks: {
+    local: {
+      url: "http://127.0.0.1:8545",
+      gasPrice: 225000000000,
+      chainId: 8178,
+      accounts: [
+        "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+      ],
+    },
+    // fuji: {
+    // 	url: "https://api.avax-test.network/ext/bc/C/rpc",
+    // 	gasPrice: 225000000000,
+    // 	chainId: 43113,
+    // 	accounts: [env.DEPLOYER_PRIVATE_KEY]
+    // },
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${env.ALCHEMY_API_KEY}`,
+      accounts: [env.DEPLOYER_PRIVATE_KEY],
+    },
+    mainnet: {
+      url: "https://api.avax.network/ext/bc/C/rpc",
+      gasPrice: 225000000000,
+      chainId: 43114,
+      accounts: [env.DEPLOYER_PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
--- fenerbahce-contract/scripts/deploy.ts
+++ fenerbahce-contract/scripts/deploy.ts
@@ -3,22 +3,23 @@ import { ethers } from "hardhat";
 async function main() {
 	let accounts = await ethers.getSigners();
 
+	// let fenerbahceAddress = "0x984D3b44841EF84a3A1b1929d7be927C99fF3c59";
 	let fenerbahceAddress = "0xFB19075D77a0F111796FB259819830F4780f1429";
 
-	if (process.env.NODE_ENV !== "mainnet") {
+	// if (process.env.NODE_ENV !== "mainnet") {
 
-		const FenerbahceToken = await ethers.getContractFactory("FBToken");
-		const fenerbahceToken = await FenerbahceToken.connect(accounts[0]).deploy([
-			"0x1070cF71bEFe2D83faE5CfD337f5A118F61F227f",
-			"0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
-			"0x1dD359fAdfB0c86a89362AF845bd3dCF416Efc92",
-			"0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
-		]);
-		await fenerbahceToken.deployed();
+	// 	const FenerbahceToken = await ethers.getContractFactory("FBToken");
+	// 	const fenerbahceToken = await FenerbahceToken.connect(accounts[0]).deploy([
+	// 		"0x1070cF71bEFe2D83faE5CfD337f5A118F61F227f",
+	// 		"0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
+	// 		"0x1dD359fAdfB0c86a89362AF845bd3dCF416Efc92",
+	// 		"0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+	// 	]);
+	// 	await fenerbahceToken.deployed();
 
-		console.log(`FBToken deployed to ${fenerbahceToken.address}`);
-		fenerbahceAddress = fenerbahceToken.address;
-	}
+	// 	console.log(`FBToken deployed to ${fenerbahceToken.address}`);
+	// 	fenerbahceAddress = fenerbahceToken.address;
+	// }
 
 	const FenerbahceAuction = await ethers.getContractFactory(
 		"FenerbahceAuction"
