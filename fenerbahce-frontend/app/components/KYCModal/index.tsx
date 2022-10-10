4cf3cf3c6ba58693eabf7a4a52bd76bed7f4cc5c
Enes İnce <inceenes10@gmail.com>
2022-10-11 01:38:48 +0300

    kyc modal form validation added

--- fenerbahce-frontend/app/components/KYCModal/index.tsx
+++ fenerbahce-frontend/app/components/KYCModal/index.tsx
@@ -11,19 +11,50 @@ import {
 	Text,
 	useDisclosure,
 } from "@chakra-ui/react";
-import { useForm } from "react-hook-form";
+import { useForm, SubmitHandler } from "react-hook-form";
 import { useConnectWallet, useKYC } from "~/context";
 import { KYCModalEventBus, loadingModalEventBus, modal1907EventBus } from "~/eventbus";
 import { GoldenFizzButton } from "../Button";
-import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 import "yup-phone";
+import gPhoneNumber from "google-libphonenumber";
 
-const schema = yup.object().shape({
+const schema = yup.object({
 	fullname: yup.string().required("İsim bilgisi zorunludur."),
-	email: yup.string().email("Email adresi geçerli değil.").required("E-posta adresi zorunludur."),
+	email: yup.string().required("E-posta bilgisi zorunludur.").email("Email adresi geçerli değil."),
 	phone: yup.string().phone("TR", false, "Telefon numarası geçerli değil.").required("Telefon numarası zorunludur."),
 });
+
+// const formatter = new AsYouTypeFormatter("US");
+
+type KYCFormType = yup.InferType<typeof schema>;
+
+const phoneUtil = gPhoneNumber.PhoneNumberUtil.getInstance();
+const PNF = gPhoneNumber.PhoneNumberFormat;
+
+// const e164ToDisplay = (e164: string): string => {
+// 	try {
+// 		const phoneNumber = phoneUtil.parse(e164);
+// 		if (phoneNumber.getCountryCode() === 90) {
+// 			return "+90 " + phoneUtil.format(phoneNumber, PNF.NATIONAL);
+// 		}
+// 		return phoneUtil.format(phoneNumber, PNF.INTERNATIONAL);
+// 	} catch (e) {
+// 		return e164;
+// 	}
+// };
+
+const AsYouTypeFormatter = gPhoneNumber.AsYouTypeFormatter;
+
+const formatPhoneNumberInternational = (rawNumber: string): string | undefined => {
+	try {
+		const phoneNumber = phoneUtil.parse(rawNumber);
+		return phoneUtil.format(phoneNumber, PNF.INTERNATIONAL);
+	} catch {
+		return undefined;
+	}
+};
 
 export const KYCModal = () => {
 	const { isOpen, onClose, onOpen } = useDisclosure();
@@ -31,8 +62,8 @@ export const KYCModal = () => {
 		handleSubmit,
 		register,
 		formState: { errors },
-	} = useForm({ resolver: yupResolver(schema) });
-	console.log(errors);
+		getValues,
+	} = useForm<KYCFormType>({ resolver: yupResolver(schema) });
 
 	KYCModalEventBus.useListener(
 		"kycmodal.open",
@@ -45,7 +76,7 @@ export const KYCModal = () => {
 	const kyc = useKYC();
 	const connectWallet = useConnectWallet();
 
-	const createKYC = async ({ fullname, email, phone }: any) => {
+	const createKYC: SubmitHandler<KYCFormType> = async ({ fullname, email, phone }: KYCFormType) => {
 		try {
 			loadingModalEventBus.publish("loadingmodal.open", { message: "KYC sistemine kaydınız ekleniyor..." });
 			const { errorMessage } = await kyc.create({ fullname, email, phone, address: connectWallet.address });
@@ -63,6 +94,8 @@ export const KYCModal = () => {
 			loadingModalEventBus.publish("loadingmodal.close");
 		}
 	};
+
+	const phoneInput = register("phone");
 
 	return (
 		<Modal isOpen={isOpen} onClose={onClose}>
@@ -85,7 +118,7 @@ export const KYCModal = () => {
 								{errors.fullname && (
 									<Text size="sm" mt="5px" color="red.400">
 										{errors.fullname.message ? <>{errors.fullname.message}</> : ""}
-										</Text>
+									</Text>
 								)}
 							</Box>
 							<Box>
@@ -100,7 +133,7 @@ export const KYCModal = () => {
 								{errors.email && (
 									<Text size="sm" mt="5px" color="red.400">
 										{errors.email.message ? <>{errors.email.message}</> : ""}
-										</Text>
+									</Text>
 								)}
 							</Box>
 							<Box>
@@ -110,12 +143,27 @@ export const KYCModal = () => {
 									placeholder="+90 (___) (___) (__) (__)"
 									_placeholder={{ color: "whiteAlpha.700" }}
 									bg="var(--indigo)"
-									{...register("phone")}
+									{...phoneInput}
+									onChange={e => {
+										phoneInput.onChange(e);
+										const formatter = new AsYouTypeFormatter("TR");
+										formatter.clear();
+										let l = "";
+										e.target.value.split("").filter(i => i !== " ").forEach(i => {
+											l = formatter.inputDigit(i);
+										});
+										const p = phoneUtil.format(
+											phoneUtil.parseAndKeepRawInput(l, "TR"),
+											gPhoneNumber.PhoneNumberFormat.INTERNATIONAL,
+										);
+										e.target.value = p.trim();
+									}}
+									onKeyUp={e => {}}
 								/>
 								{errors.phone && (
 									<Text size="sm" mt="5px" color="red.400">
 										{errors.phone.message ? <>{errors.phone.message}</> : ""}
-										</Text>
+									</Text>
 								)}
 							</Box>
 							<GoldenFizzButton type="submit">SUBMIT</GoldenFizzButton>
