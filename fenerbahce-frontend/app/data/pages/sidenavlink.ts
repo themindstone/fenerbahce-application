332d201e50886e6a8bcc63755e844c7b211e3c89
Enes İnce <inceenes10@gmail.com>
2022-10-24 20:25:15 +0300

    fix

--- fenerbahce-frontend/app/data/pages/sidenavlink.ts
+++ fenerbahce-frontend/app/data/pages/sidenavlink.ts
@@ -1,29 +1,24 @@
 import type { ISideNavLink } from "~/interfaces";
 
 export const sidenavlink: ISideNavLink[] = [
-    {
-        id: "45f7b74e-79e8-47a5-8496-4341384a0de5",
-        href: "/",
-        text: "HAKKINDA",
-    },
+    // {
+    //     id: "45f7b74e-79e8-47a5-8496-4341384a0de5",
+    //     href: "/",
+    //     text: "HAKKINDA",
+    // },
     {
         id: "ae10ef7d-ace6-4552-bca6-beda9738f375",
         href: "/",
         text: "FAYDALAR",
     },
-    {
-        id: "7430d629-2031-40d5-8a86-c8acb6e8bd30",
-        href: "/",
-        text: "ŞARTLAR",
-    },
+    // {
+    //     id: "7430d629-2031-40d5-8a86-c8acb6e8bd30",
+    //     href: "/",
+    //     text: "ŞARTLAR",
+    // },
     {
         id: "93808157-ed22-4c15-83e5-e64fabe64692",
         href: "/",
         text: "S.S.S",
-    },
-    {
-        id: "f3fc44af-5a7e-4416-b998-a3990d9a3f3b",
-        href: "/",
-        text: "ENGLISH",
     },
 ];
