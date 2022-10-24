87cba09b69a617ba04c4c8cdd06b1c4a4e6d4ea1
Enes İnce <inceenes10@gmail.com>
2022-10-25 01:48:31 +0300

    fix

--- fenerbahce-frontend/app/pages/Admin/CreateAuction/schema.ts
+++ fenerbahce-frontend/app/pages/Admin/CreateAuction/schema.ts
@@ -21,13 +21,13 @@ const schema = yup.object({
 	buyNowPrice: yup.number().typeError("Satın al fiyatı sayı olmalıdır").required("Satın al fiyatı zorunludur."),
 	startDate: yup
 		.date()
-		.typeError("Başlangıç tarihi tarih olmalıdır")
+		.typeError("Başlangıç tarihi geçerli bir tarih olmalıdır")
 		.required("Başlangıç tarihi zorunludur.")
 		.when([], (schema: any) => schema.min(new Date(), "Başlangıç tarihi bugünden ileri olmalıdır."))
 		.when(["endDate"], (endDate: Date, schema: any) =>
 			schema.max(endDate, "Başlangıç tarihi bitiş tarihinden önce olmalıdır."),
 		),
-	endDate: yup.date().typeError("Başlangıç tarihi tarih olmalıdır").required("Bitiş tarihi zorunludur."),
+	endDate: yup.date().typeError("Bitiş tarihi geçerli bir tarih olmalıdır").required("Bitiş tarihi zorunludur."),
 	photoUrls: yup
 		.array()
 		.of(
