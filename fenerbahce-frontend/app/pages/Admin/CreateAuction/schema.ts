28b79367e166bb4d676e0f93037ff3ee3e30c84a
Enes İnce <inceenes10@gmail.com>
2022-10-25 01:23:33 +0300

    fix

--- fenerbahce-frontend/app/pages/Admin/CreateAuction/schema.ts
+++ fenerbahce-frontend/app/pages/Admin/CreateAuction/schema.ts
@@ -6,25 +6,28 @@ const schema = yup.object({
 	name: yup.string().required("İsim bilgisi zorunludur"),
 	startPrice: yup
 		.number()
+		.typeError("Başlangıç fiyatı sayı olmalıdır")
 		.required("Başlangıç fiyatı zorunludur.")
 		.when("buyNowPrice", (buyNowPrice: number, schema: any) =>
 			schema.max(buyNowPrice, "Başlangıç fiyatı bitiş fiyatından büyük olamaz."),
 		),
 	bidIncrement: yup
 		.number()
+		.typeError("Artış miktarı sayı olmalıdır")
 		.required("Artış miktarı zorunludur.")
 		.when(["startPrice"], (startPrice: any, schema: any) =>
 			schema.max(startPrice, "Artış miktarı başlangıç fiyatından büyük olamaz."),
 		),
-	buyNowPrice: yup.number().required("Satın al fiyatı zorunludur."),
+	buyNowPrice: yup.number().typeError("Satın al fiyatı sayı olmalıdır").required("Satın al fiyatı zorunludur."),
 	startDate: yup
 		.date()
+		.typeError("Başlangıç tarihi tarih olmalıdır.")
 		.required("Başlangıç tarihi zorunludur.")
 		.when([], (schema: any) => schema.min(new Date(), "Başlangıç tarihi bugünden ileri olmalıdır."))
 		.when(["endDate"], (endDate: Date, schema: any) =>
 			schema.max(endDate, "Başlangıç tarihi bitiş tarihinden önce olmalıdır."),
 		),
-	endDate: yup.date().required("Bitiş tarihi zorunludur."),
+	endDate: yup.date().typeError("Bitiş tarihi tarih olmalıdır.").required("Bitiş tarihi zorunludur."),
 	photoUrls: yup
 		.array()
 		.of(yup.object({ photoUrl: yup.string().required("Fotoğraf url'si zorunludur.") }))
